db32d50b6e1dbe7b258932475eb065c4693f00ea
Vamshidhar Reddy Gummi <prasannapatireddy1984@gmail.com>
2026-06-21 09:29:46 +0530

    review after order complete

--- src/main/java/com/rentacostume/reviews/config/KafkaProducerConfig.java
+++ src/main/java/com/rentacostume/reviews/config/KafkaProducerConfig.java
@@ -16,12 +16,27 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrap;
 
+    @Value("${spring.kafka.properties.security.protocol:SASL_SSL}")
+    private String securityProtocol;
+
+    @Value("${spring.kafka.properties.sasl.mechanism:PLAIN}")
+    private String saslMechanism;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String saslJaasConfig;
+
     @Bean
     public ProducerFactory<String, String> stringProducerFactory() {
         Map<String, Object> props = new HashMap<>();
+
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        props.put("security.protocol", securityProtocol);
+        props.put("sasl.mechanism", saslMechanism);
+        props.put("sasl.jaas.config", saslJaasConfig);
+
         return new DefaultKafkaProducerFactory<>(props);
     }
 
